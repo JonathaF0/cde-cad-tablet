891a607cf149f208f9a027015fb00aa066699495
JonathaF0 <110953872+JonathaF0@users.noreply.github.com>
2026-02-06 16:54:34 -0500

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,21 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'CDE Inc.'
+description 'In-Game CAD Tablet Interface'
+version '1.0.0'
+
+client_scripts {
+    'client/*.lua'
+}
+-- Server scripts
+server_scripts {
+    'server/*.lua'
+}
+ui_page 'html/index.html'
+
+files {
+    'html/index.html',
+    'html/style.css',
+    'html/script.js'
+}
